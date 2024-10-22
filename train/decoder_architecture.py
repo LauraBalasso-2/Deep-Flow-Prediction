798075ce31f555c38b756f2dd2ca16ec64928517
LauraBalasso-2 <laura.balasso@optimad.it>
2024-10-22 14:31:30 +0200

    core: double the number of layers in decoder

--- train/decoder_architecture.py
+++ train/decoder_architecture.py
@@ -43,20 +43,22 @@ class Decoder(nn.Module):
         # Convolutional layer to transform input (N, 1, 1) into (512, 2, 2)
         self.conv_input = nn.Conv2d(input_size, channels * 8, kernel_size=1, stride=1, padding=0)
 
-        # Decoder layers (only upsampling parts, transposed=True)
-        self.dlayer6 = blockUNet(channels * 8, channels * 8, 'dlayer6', transposed=True, bn=True, relu=True,
-                                 dropout=dropout, size=2, pad=0)
-        self.dlayer5 = blockUNet(channels * 8, channels * 8, 'dlayer5', transposed=True, bn=True, relu=True,
-                                 dropout=dropout, size=2, pad=0)
-        self.dlayer4 = blockUNet(channels * 8, channels * 4, 'dlayer4', transposed=True, bn=True, relu=True,
-                                 dropout=dropout)
-        self.dlayer3 = blockUNet(channels * 4, channels * 2, 'dlayer3', transposed=True, bn=True, relu=True,
-                                 dropout=dropout)
-        self.dlayer2b = blockUNet(channels * 2, channels * 2, 'dlayer2b', transposed=True, bn=True, relu=True,
-                                  dropout=dropout)
-        self.dlayer2 = blockUNet(channels * 2, channels, 'dlayer2', transposed=True, bn=True, relu=True,
-                                 dropout=dropout)
+        # Remaining decoder layers (with dlayer6_extra removed)
+        self.dlayer6 = blockUNet(channels * 8, channels * 8, 'dlayer6', transposed=True, bn=True, relu=True, size=2, pad=0)
 
+        self.dlayer5 = blockUNet(channels * 8, channels * 8, 'dlayer5', transposed=True, bn=True, relu=True, size=2, pad=0)
+        self.dlayer5_extra = blockUNet(channels * 8, channels * 8, 'dlayer5_extra', transposed=True, bn=True, relu=True, size=2, pad=0)
+
+        self.dlayer4 = blockUNet(channels * 8, channels * 4, 'dlayer4', transposed=True, bn=True, relu=True, size=4, pad=1)
+        self.dlayer4_extra = blockUNet(channels * 4, channels * 4, 'dlayer4_extra', transposed=True, bn=True, relu=True, size=4, pad=1)
+
+        self.dlayer3 = blockUNet(channels * 4, channels * 2, 'dlayer3', transposed=True, bn=True, relu=True, size=4, pad=1)
+        self.dlayer3_extra = blockUNet(channels * 2, channels * 2, 'dlayer3_extra', transposed=True, bn=True, relu=True, size=4, pad=1)
+
+        self.dlayer2b = blockUNet(channels * 2, channels * 2, 'dlayer2b', transposed=True, bn=True, relu=True, size=4, pad=1)
+        self.dlayer2b_extra = blockUNet(channels * 2, channels * 2, 'dlayer2b_extra', transposed=True, bn=True, relu=True, size=4, pad=1)
+
+        self.dlayer2 = blockUNet(channels * 2, channels, 'dlayer2', transposed=True, bn=True, relu=True, size=4, pad=1)
         self.dlayer1 = blockUNet(channels, 3, 'dlayer1', transposed=True, bn=False, relu=True)
         self.dlayer0 = nn.Conv2d(4, 3, kernel_size=3, stride=1, padding=1, bias=False)
 
@@ -66,11 +68,20 @@ class Decoder(nn.Module):
 
         # Pass through decoder layers
         dout6 = self.dlayer6(x)
+
         dout5 = self.dlayer5(dout6)
-        dout4 = self.dlayer4(dout5)
-        dout3 = self.dlayer3(dout4)
-        dout2b = self.dlayer2b(dout3)
-        dout2 = self.dlayer2(dout2b)
+        dout5_extra = self.dlayer5_extra(dout5)
+
+        dout4 = self.dlayer4(dout5_extra)
+        dout4_extra = self.dlayer4_extra(dout4)
+
+        dout3 = self.dlayer3(dout4_extra)
+        dout3_extra = self.dlayer3_extra(dout3)
+
+        dout2b = self.dlayer2b(dout3_extra)
+        dout2b_extra = self.dlayer2b_extra(dout2b)
+
+        dout2 = self.dlayer2(dout2b_extra)
         dout1 = self.dlayer1(dout2)
         dout0 = self.dlayer0(torch.cat([dout1, sdf], dim=1))
 

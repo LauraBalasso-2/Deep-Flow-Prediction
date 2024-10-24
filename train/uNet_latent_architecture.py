01511fffcd26d43eaa55a54c5c7cbc1ea61a3fdf
LauraBalasso-2 <laura.balasso@optimad.it>
2024-10-24 11:51:07 +0200

    fix: correct layers size

--- train/uNet_latent_architecture.py
+++ train/uNet_latent_architecture.py
@@ -44,6 +44,7 @@ class UNet(nn.Module):
     def __init__(self, latent_size, channelExponent=6, dropout=0.):
         super(UNet, self).__init__()
         channels = int(2 ** channelExponent + 0.5)
+        self.latent_size = latent_size
 
         self.layer1 = nn.Sequential()
         self.layer1.add_module('layer1_conv', nn.Conv2d(2, channels, 4, 2, 1, bias=True))
@@ -54,22 +55,21 @@ class UNet(nn.Module):
                                  dropout=dropout)
         self.layer3 = blockUNet(channels * 2, channels * 4, 'layer3', transposed=False, bn=True, relu=False,
                                 dropout=dropout)
-
-        # Progressively reduce spatial size to 1x1
+        # note the following layer also had a kernel size of 2 in the original version (cf
+        # https://arxiv.org/abs/1810.08217) it is now changed to size 4 for encoder/decoder symmetry; to reproduce
+        # the old/original results, please change it to 2
         self.layer4 = blockUNet(channels * 4, channels * 8, 'layer4', transposed=False, bn=True, relu=False,
-                                dropout=dropout)
+                                dropout=dropout, size=4)  # note, size 4!
         self.layer5 = blockUNet(channels * 8, channels * 8, 'layer5', transposed=False, bn=True, relu=False,
-                                dropout=dropout, size=4)
-        self.layer6 = blockUNet(channels * 8, channels * 8, 'layer6', transposed=False, bn=False, relu=False,
-                                dropout=dropout, size=2)
+                                dropout=dropout, size=2, pad=0)
+        #self.layer6 = blockUNet(channels * 8, channels * 8, 'layer6', transposed=False, bn=False, relu=False,
+        #                        dropout=dropout, size=2, pad=0)
 
-        # Bottleneck removed, and we start decoding from the provided matrix (fixed latent input)
-        self.dlayer_bottleneck = nn.ConvTranspose2d(latent_size, channels * 8, kernel_size=2, stride=1, padding=0)
-
-        self.dlayer6 = blockUNet(channels * 8, channels * 8, 'dlayer6', transposed=True, bn=True, relu=True,
-                                 dropout=dropout, size=2)
+        # note, kernel size is internally reduced by one now
+        self.dlayer6 = blockUNet(self.latent_size, channels * 8, 'dlayer6', transposed=True, bn=True, relu=True,
+                                 dropout=dropout, size=2, pad=0)
         self.dlayer5 = blockUNet(channels * 16, channels * 8, 'dlayer5', transposed=True, bn=True, relu=True,
-                                 dropout=dropout, size=2)
+                                 dropout=dropout, size=2, pad=0)
         self.dlayer4 = blockUNet(channels * 16, channels * 4, 'dlayer4', transposed=True, bn=True, relu=True,
                                  dropout=dropout)
         self.dlayer3 = blockUNet(channels * 8, channels * 2, 'dlayer3', transposed=True, bn=True, relu=True,
@@ -78,33 +78,31 @@ class UNet(nn.Module):
                                   dropout=dropout)
         self.dlayer2 = blockUNet(channels * 4, channels, 'dlayer2', transposed=True, bn=True, relu=True,
                                  dropout=dropout)
+
         self.dlayer1 = blockUNet(channels * 2, 4, 'dlayer1', transposed=True, bn=False, relu=True)
 
     def forward(self, x, latent_code):
-        out1 = self.layer1(x)
-        out2 = self.layer2(out1)
-        out2b = self.layer2b(out2)
-        out3 = self.layer3(out2b)
-        out4 = self.layer4(out3)
-        out5 = self.layer5(out4)
-        out6 = self.layer6(out5)
+            out1 = self.layer1(x)
+            out2 = self.layer2(out1)
+            out2b = self.layer2b(out2)
+            out3 = self.layer3(out2b)
+            out4 = self.layer4(out3)
+            out5 = self.layer5(out4)
+            out6 = latent_code
+            dout6 = self.dlayer6(out6)
 
-        # Instead of the bottleneck, we use the fixed_matrix
-        # Ensure fixed_matrix has shape [batch_size, latent_size, 1, 1]
-        dout_bottleneck = self.dlayer_bottleneck(latent_code)
+            dout6_out5 = torch.cat([dout6, out5], 1)
+            dout5 = self.dlayer5(dout6_out5)
+            dout5_out4 = torch.cat([dout5, out4], 1)
+            dout4 = self.dlayer4(dout5_out4)
+            dout4_out3 = torch.cat([dout4, out3], 1)
+            dout3 = self.dlayer3(dout4_out3)
+            dout3_out2b = torch.cat([dout3, out2b], 1)
+            dout2b = self.dlayer2b(dout3_out2b)
+            dout2b_out2 = torch.cat([dout2b, out2], 1)
+            dout2 = self.dlayer2(dout2b_out2)
+            dout2_out1 = torch.cat([dout2, out1], 1)
+            dout1 = self.dlayer1(dout2_out1)
 
-        dout6 = self.dlayer6(dout_bottleneck)
-        dout6_out5 = torch.cat([dout6, out6], 1)
-        dout5 = self.dlayer5(dout6_out5)
-        dout5_out4 = torch.cat([dout5, out4], 1)
-        dout4 = self.dlayer4(dout5_out4)
-        dout4_out3 = torch.cat([dout4, out3], 1)
-        dout3 = self.dlayer3(dout4_out3)
-        dout3_out2b = torch.cat([dout3, out2b], 1)
-        dout2b = self.dlayer2b(dout3_out2b)
-        dout2b_out2 = torch.cat([dout2b, out2], 1)
-        dout2 = self.dlayer2(dout2b_out2)
-        dout2_out1 = torch.cat([dout2, out1], 1)
-        dout1 = self.dlayer1(dout2_out1)
-        return dout1
+            return dout1
 
